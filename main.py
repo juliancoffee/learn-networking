7db70908caff3914dfc3e28ad3e3f51a5405c68e
juliancoffee <lightdarkdaughter@gmail.com>
2024-09-09 14:09:56 +0300

    MVP

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def root(request: Request):
+    print(request.client)
+    return {
+        "ip": request.client.host,
+        "port": request.client.port,
+    }
