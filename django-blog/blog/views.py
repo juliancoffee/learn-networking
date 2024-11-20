f0a47c25eb6c721f742ce48f7343311c40f4e51a
juliancoffee <lightdarkdaughter@gmail.com>
2024-11-20 20:34:59 +0200

    Return json response instead

--- django-blog/blog/views.py
+++ django-blog/blog/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from .models import Post
 
 # Create your views here.
 def index(request) -> HttpResponse:
     posts = Post.objects.order_by("pub_date")
-    page = " ".join([f"{p.id}) {p.post_text}" for p in posts])
-    return HttpResponse(page)
+    response = JsonResponse({"posts": [
+        {"id": p.id, "text": p.post_text} for p in posts]
+    })
+    return response
 
 def read(request, post_id):
     p = Post.objects.get(pk=post_id)
-    comments = " * ".join(c.comment_text for c in p.comment_set.all())
-    page = f"{p.post_text} <> {comments}"
-    return HttpResponse(page)
+    response = JsonResponse({
+        "post": p.post_text,
+        "comments": [
+            c.comment_text for c in p.comment_set.all()
+        ],
+    })
+    return response
