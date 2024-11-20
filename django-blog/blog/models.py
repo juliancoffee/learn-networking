8032c0251e8335fbc1a03f5191e685d52c9be07a
juliancoffee <lightdarkdaughter@gmail.com>
2024-11-20 19:32:18 +0200

    Add database configuration and two models

--- django-blog/blog/models.py
+++ django-blog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    post_text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField("publishing date")
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("publishing date")
