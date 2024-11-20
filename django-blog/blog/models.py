27ba2a185b4626eeb57e0baa358eeefe276d0e9c
juliancoffee <lightdarkdaughter@gmail.com>
2024-11-20 20:30:17 +0200

    Added some views there

--- django-blog/blog/models.py
+++ django-blog/blog/models.py
@@ -4,7 +4,13 @@ class Post(models.Model):
     post_text = models.CharField(max_length=500)
     pub_date = models.DateTimeField("publishing date")
 
+    def __str__(self) -> str:
+        return self.post_text
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     comment_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("publishing date")
+
+    def __str__(self) -> str:
+        return self.comment_text
--- django-blog/blog/views.py
+++ django-blog/blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
 def index(request) -> HttpResponse:
-    return HttpResponse("hi there")
+    posts = Post.objects.order_by("pub_date")
+    page = " ".join([f"{p.id}) {p.post_text}" for p in posts])
+    return HttpResponse(page)
+
+def read(request, post_id):
+    p = Post.objects.get(pk=post_id)
+    comments = " * ".join(c.comment_text for c in p.comment_set.all())
+    page = f"{p.post_text} <> {comments}"
+    return HttpResponse(page)
