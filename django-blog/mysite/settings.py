116825db20b9b226b3ea450e80df982b0218b9f3
juliancoffee <lightdarkdaughter@gmail.com>
2024-11-20 13:54:51 +0200

    Add blog
    + Add Django mypy plugin

--- django-blog/mysite/settings.py
+++ django-blog/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3x9tp!e%2(3=2=v^8=ub^k-ue5hbas8$zp67_ly-@m0d%szdr2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
