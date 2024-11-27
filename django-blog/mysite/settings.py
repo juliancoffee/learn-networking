ec685ed9c349e409fc6f8cd4c5f3a042f5d2fffd
juliancoffee <lightdarkdaughter@gmail.com>
2024-11-27 20:39:15 +0200

    Add django debug toolbar

--- django-blog/mysite/settings.py
+++ django-blog/mysite/settings.py
@@ -28,12 +28,16 @@ SECRET_KEY = (
 DEBUG = True
 
 ALLOWED_HOSTS: list[str] = []
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
+    "debug_toolbar",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
